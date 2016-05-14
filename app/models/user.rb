e1e28443d67cad0310fd90274ca3ee9368e54bc7
Michael Wang <wanghk@aceworld.com.au>
2016-05-15 03:52:57 +1000

    Add Stripe.js during registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
-  after_create :sign_up_for_mailing_list
+  before_create :make_payment, unless: Proc.new { |user| user.admin? }
+  # after_create :sign_up_for_mailing_list
+  attr_accessor :stripe_token
 
   def set_default_role
     self.role ||= :user
@@ -11,6 +13,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def make_payment
+    MakePaymentService.new.perform(self)
+  end
 
   def sign_up_for_mailing_list
     MailingListSignupJob.perform_later(self)
