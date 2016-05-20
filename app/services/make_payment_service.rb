262a4361af9d1c9b5e82e625249302098ce13920
Michael Wang <wanghk@aceworld.com.au>
2016-05-20 13:14:20 +1000

    Change makepayment_service.rb from one time payment to subscription

--- app/services/make_payment_service.rb
+++ app/services/make_payment_service.rb
@@ -6,9 +6,17 @@ class MakePaymentService
       raise ActiveRecord::RecordInvalid.new(user)
     end
     customer = create_customer(user)
-    charge = create_charge(customer)
+    subscription = create_subscribe(customer)
+
+    # Charge works for one time payment
+    # charge = create_charge(customer)
+
     user.stripe_token = nil
-    Rails.logger.info("Stripe transaction for #{user.email}") if charge[:paid] == true
+    Rails.logger.info("Stripe subscription for #{user.email}") if subscription[:status] == 'active'
+
+    # Print logger for charge if charge done
+    # Rails.logger.info("Stripe transaction for #{user.email}") if charge[:paid] == true
+
   rescue Stripe::InvalidRequestError => e
     user.errors[:base] << e.message
     user.stripe_token = nil
@@ -19,22 +27,35 @@ class MakePaymentService
     raise ActiveRecord::RecordInvalid.new(user)
   end
 
+  # This works for create a user in Stripe
   def create_customer(user)
     customer = Stripe::Customer.create(
       :email => user.email,
-      :card => user.stripe_token
+      # Add for test payment
+      :source => user.stripe_token,
+      :plan => "oq_membership"
+      # Add for test payment
+      # :card => user.stripe_token
     )
   end
 
-  def create_charge(customer)
-    price = Rails.application.secrets.product_price
-    title = Rails.application.secrets.product_title
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => "#{price}",
-      :description => "#{title}",
-      :currency    => "aud"
+  def create_subscribe(customer)
+    Stripe::Subscription.create(
+    :customer   => customer.id,
+    :plan       => "oq_membership"
     )
   end
+
+  # This area for on time payment
+  # def create_charge(customer)
+  #   price = Rails.application.secrets.product_price
+  #   title = Rails.application.secrets.product_title
+  #   charge = Stripe::Charge.create(
+  #     :customer    => customer.id,
+  #     :amount      => "#{price}",
+  #     :description => "#{title}",
+  #     :currency    => "aud"
+  #   )
+  # end
 
 end
